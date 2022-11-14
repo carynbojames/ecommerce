95ab2bdc3fff273c36c79556b44b8e87f162afc7
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-11-13 20:03:07 -0600

    Updated code

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,26 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const productData = await Product.findAll()
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const productData = await Product.findByPk(req.params.id)
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
